f84970210f582f9bd7c66093e66aaf1ec3cd513f
Vidupa Senevirathna <vidupasenevirathna29@gmail.com>
2026-04-21 22:51:44 +0530

    Cleanup: remove unused import in GlobalExceptionMapper

--- src/main/java/com/smartcampus/mapper/GlobalExceptionMapper.java
+++ src/main/java/com/smartcampus/mapper/GlobalExceptionMapper.java
@@ -1,6 +1,5 @@
 package com.smartcampus.mapper;
 
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
