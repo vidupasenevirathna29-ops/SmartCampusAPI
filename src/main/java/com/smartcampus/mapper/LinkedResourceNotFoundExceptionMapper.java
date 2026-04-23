5a7f56a7b6866f01e9dd70a0e42a2ba67ba9b2cf
Vidupa Senevirathna <vidupasenevirathna29@gmail.com>
2026-04-24 04:18:16 +0530

    refactor: humanize comments in LinkedResourceNotFoundExceptionMapper

--- src/main/java/com/smartcampus/mapper/LinkedResourceNotFoundExceptionMapper.java
+++ src/main/java/com/smartcampus/mapper/LinkedResourceNotFoundExceptionMapper.java
@@ -8,14 +8,14 @@ import javax.ws.rs.ext.Provider;
 import java.util.Map;
 
 /**
- * Exception mapper for LinkedResourceNotFoundException.
- * Maps to HTTP 422 Unprocessable Entity.
+ * Handles the case where a sensor is being created with a roomId that doesn't exist.
+ * We return 422 Unprocessable Entity because the request is valid JSON but the data doesn't make sense.
  */
 @Provider
 public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
     @Override
     public Response toResponse(LinkedResourceNotFoundException exception) {
-        return Response.status(422) // 422 Unprocessable Entity
+        return Response.status(422) // 422 means the request was understood but the linked resource wasn't found
                 .entity(Map.of(
                         "error", "Unprocessable Entity",
                         "message", exception.getMessage(),
